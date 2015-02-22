aa1c92bc56af2680518e99dca5cb7b1e1e4bfb96
wolfcall <gmathieu6@gmail.com>
2015-02-21 19:08:04 -0500

    progress

--- app/src/main/java/myproject2/snake2/ButtonsActivities.java
+++ app/src/main/java/myproject2/snake2/ButtonsActivities.java
@@ -2,6 +2,8 @@ package myproject2.snake2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 /**
@@ -15,5 +17,11 @@ public class ButtonsActivities extends Activity{
         setContentView(R.layout.activity_main);
 
         final Button game = (Button) findViewById(R.id.btnStart);
+        game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.w("Status", "game button clicked");
+            }
+        });
     }
 }
